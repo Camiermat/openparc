d28ae62d8fb0cbbb9836f4a5b5095543af2f3ce8
root <timdaurelle@gmail.com>
2020-01-16 10:44:13 +0100

    finish menu

---
+++ web/views/v_billeterie.php
@@ -0,0 +1,13 @@
+<?php
+/*
+
+ */
+//  En tête de page
+//require(PATH_ENTITY.'Photo.php');
+//require(PATH_ENTITY.'Categorie.php');
+?>
+<?php require_once(PATH_VIEWS.'header.php');?>
+
+<!--  Zone message d'alerte -->
+<?php require_once(PATH_VIEWS.'alert.php');?>
+
